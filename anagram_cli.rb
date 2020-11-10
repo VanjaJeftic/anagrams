6bb1da3345f2af4651031d8c39bc20651c937b7d
Vanja <vanjajeftic97@gmail.com>
2020-11-10 16:05:29 +0100

    tests are added

--- anagram_cli.rb
+++ anagram_cli.rb
@@ -1,14 +1,16 @@
 require_relative 'lib/anagrams'
 
 print 'Enter the file: '
-file = gets.chomp
+input = gets.chomp
 puts ''
 
-# begin
-anagrams = Anagrams.new(file)
-# rescue StandardError => e
-#  puts "An error of type #{e.class} happened, message is: #{e.message}"
-#  exit
-# end
+begin
+  anagrams = Anagrams.new(input)
+rescue StandardError => e
+  puts "An error of type #{e.class} happened, message is: #{e.message}"
+  exit
+end
 
-puts anagrams.to_s
+anagrams.find_anagrams.values.select { |word_values| word_values.length > 1 }.each do |items|
+  puts items.join(' ')
+end
--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,14 +1,7 @@
 class Anagrams
-  def initialize(text_file)
-    @text_file = text_file
-  end
-
-  def to_s
-    anagram_pairs = []
-    find_anagrams.values.select { |word_values| word_values.length > 1 }.each do |item|
-      anagram_pairs.push(item.join(' '))
-    end
-    anagram_pairs
+  def initialize(input)
+    check_input(input)
+    @input = input
   end
 
   def find_anagrams
@@ -26,15 +19,18 @@ class Anagrams
     anagrams_dictionary
   end
 
-  # def check_file_extension(text_file)
-  #  raise StandardError, 'Failed to open the file or file does not exist!' if File.extname(text_file) == '.zip'
-  # end
+  private
+
+  def check_input(input)
+    raise StandardError, 'Input is empty or file does not exist!' if input == '' ||
+                                                                     File.basename(input) != 'test1.txt' &&
+                                                                     File.basename(input) != 'test2.txt' &&
+                                                                     File.basename(input) != 'test3.txt' &&
+                                                                     File.basename(input) != 'test4.txt' &&
+                                                                     File.basename(input) != 'wordlist.txt'
+  end
 
   def anagrams_checker
-    if @text_file.is_a?(Array)
-      @text_file
-    else
-      File.read(@text_file).encode('UTF-8', invalid: :replace, replace: '').split("\n")
-    end
+    File.read(@input).encode('UTF-8', invalid: :replace, replace: '').split("\n")
   end
 end
